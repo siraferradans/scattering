5834e0da773d029f69e995caa365ed8aa726b73a
siraferradans <sira.ferradans@gmail.com>
2016-06-01 16:01:54 +0200

    First commit

---
+++ scattering/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scattering',
+      version='0.1',
+      description='Implementation of the scattering transform',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
