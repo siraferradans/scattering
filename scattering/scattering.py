a6ccce098a0008d0d4051c04cd9618d4979ce87a
siraferradans <sira.ferradans@gmail.com>
2016-08-22 17:06:09 +0200

    Create internal functions and comment them

--- scattering/scattering.py
+++ scattering/scattering.py
@@ -18,32 +18,66 @@ def apply_fourier_mult(signals,filters):
     return filtered_signals
 
 
-def subsample(X,j):
-    #assume X is in the spatial domain with first two
-    #dims images, angle
+def _subsample(X,j):
+    """
+    Spatial subsampling on the last two dimensions of X at a rate that depends on 2**j.
+
+    Parameters
+    ----------
+    X : array like variable.
+        3D dnarray with shape (N,L,n,n) or (N,n,n). The subsampling is produced in the last two
+        dimensions (n,n)
+    j : int
+        Rate of subsampling is 2**j
+
+    Returns
+    -------
+    XX : ndarray
+        Subsampled images
+    """
+
+    dsf = 2**j
     
-    N = X.shape[3]
-    dsf = 2**(j)
+    return dsf*X[..., ::dsf, ::dsf]
+
+
+def _apply_lowpass(img, phi, J, n_scat):
+    """Applies a low pass filter on the 'img' set of 2D arrays and subsamples.
+
+    Convolution the filter phi (in the Fourier domain) on the set of images defined by img. The convolution is done in
+    the Fourier domain, thus it is a pointwise multiplication of the filter phi and the images, stored in
+    the last 2 dimensions of img. Then, the images are subsampled at the appropiate rate to have 'n_scat'
+    spatial coefficients
+
+    Parameters
+    ----------
+    img : ndarray of (N,L,n,n) or (N,n,n) shape.
+        Input images in the spatial domain, stacked along the first dimensions.
+    phi : Low pass filter as a 2D ndarray
+        Low pass filter in the Fourier domain, stored as (n,n) matrix
+    J : int
+        Rate of subsampling 2**J
+    n_scat : int
+        number of spatial coefficients of the scattering vector
+
+    Returns
+    -------
+    XX : ndarray
+        stacked images after being filtered and subsampled
+    """
+
+    img_filtered = np.real(np.fft.ifft2(np.fft.fft2(img)*phi))
+
+    n_nolp = img.shape[-1]
+
+    ds = int(n_nolp / n_scat)
     
-    return 2**(j)*X[...,::dsf,::dsf]
-
-
-def apply_lowpass(img, phi, J, N_scat):
-
-    Img_filtered = np.real(np.fft.ifft2(np.fft.fft2(img)*phi))
-#    ds = 2**(J-1) # -1 for the oversampling, to be sure that everything is ok
-    N_nolp = img.shape[-1]
-
-    N_nolp_r = N_nolp // 2 + 1
-    ds = int(N_nolp / N_scat)
-    
-    return 2 ** (J - 1) *Img_filtered[...,::ds,::ds]
+    return 2 ** (J - 1) * img_filtered[..., ::ds, ::ds]
 
 
 def scattering(x,wavelet_filters=None,m=2):
 
-    """
-    Compute the scattering transform of a signal (or set of signals).
+    """Compute the scattering transform of a signal (or set of signals).
 
     Given 'x', a set of 2D signals, this function computes the scattering transform
     of these signals using the filter bank 'wavelet_filters'.
@@ -123,7 +157,7 @@ def scattering(x,wavelet_filters=None,m=2):
     current_resolution = 0
 
     #Zero order coeffs
-    S[:,0,:,:] = apply_lowpass(x, wavelet_filters['phi'][current_resolution], J,  spatial_coefs)
+    S[:, 0, :, :] = _apply_lowpass(x, wavelet_filters['phi'][current_resolution], J,  spatial_coefs)
 
     # First order scattering coeffs
     if m>0:
@@ -138,12 +172,12 @@ def scattering(x,wavelet_filters=None,m=2):
             v_resolution.append(resolution)
 
             # fft2(| x conv Psi_j |): X is full resolution, as well as the filters
-            aux = subsample(np.fft.ifft2(apply_fourier_mult(X,filtersj)), resolution )
+            aux = _subsample(np.fft.ifft2(apply_fourier_mult(X,filtersj)), resolution )
 
             V.append( aux )
             U.append( np.abs(aux))
 
-            Sview[:, j, :, :, :] = apply_lowpass(U[j], wavelet_filters['phi'][resolution], J,  spatial_coefs)
+            Sview[:, j, :, :, :] = _apply_lowpass(U[j], wavelet_filters['phi'][resolution], J,  spatial_coefs)
 
     # Second order scattering coeffs
     if m>1:
@@ -165,7 +199,7 @@ def scattering(x,wavelet_filters=None,m=2):
                     aux = np.abs(np.fft.ifft2(apply_fourier_mult(Ujl1, wavelet_filters['psi'][current_resolution][j2])))
                     # computing all angles at once
                     S2norder[:,indx,:,:,:]= \
-                        apply_lowpass(aux, wavelet_filters['phi'][current_resolution], J,  spatial_coefs)
+                        _apply_lowpass(aux, wavelet_filters['phi'][current_resolution], J,  spatial_coefs)
                     
                     indx = indx+1
     
@@ -173,10 +207,8 @@ def scattering(x,wavelet_filters=None,m=2):
 
     return S,U
 
-
+"""
 def apply_lowpass_fast(img, phi, J, N_scat):
-    # NB: I could compute N_scat here, but in case we want to oversample, this
-    # may be useful. I should make a class.
 
     #This function can be applied to a set of images and angles, the input img is assumed
     #to be of size (num_images,L,size_x,size_y)
@@ -196,7 +228,7 @@ def apply_lowpass_fast(img, phi, J, N_scat):
     return out
 
 
-"""
+
 def apply_lowpass_ivan(img, phi, J, N_scat,type_complex='complex64'):
     
     #fft, ifft, fft2, ifft2, rfft, irfft, rfft2, irfft2 = select_fft(fft_choice)
@@ -236,67 +268,4 @@ def apply_lowpass_ivan(img, phi, J, N_scat,type_complex='complex64'):
     out = 2**(J - 1) * np.real(out)
     
     return out
-"""
-
-"""
-Scattering with no subsampling
-def scattering(x,wavelet_filters,m):
-
-    num_signals = x.shape[0]
-    J = len(wavelet_filters['psi'])    #number of scales
-    L = len(wavelet_filters['psi'][0]) #number of orientations
-    
-    if (m>1):
-        num_coefs = 1+J*L+J*(J-1)*L**2/2
-    else: 
-        num_coefs = 1+J*L
-        
-        
-    spatial_coefs = x.shape[1] / 2 ** (J-1)
-
-    U = []
-    V = []
-    S = np.ndarray((num_signals,num_coefs,spatial_coefs,spatial_coefs))
-
-    #Zero order coeffs
-    S[:,0,:,:]= apply_lowpass(x, wavelet_filters['phi'][0], J,  spatial_coefs)
-
-    #First order scattering coeffs
-    Sview = S[:,1:J*L+1,:,:].view()
-    Sview.shape=(num_signals,J,L,spatial_coefs,spatial_coefs)
-
-    X = np.fft.fft2(x) # precompute the fourier transform of the images
-    for j in np.arange(J):
-        filtersj = wavelet_filters['psi'][j].view()
-        # fft2(| x conv Psi_j |)
-        aux = subsample(np.fft.ifft2(apply_fourier_mult(X,filtersj)), j )
-        V.append( aux )
-        U.append( np.abs(aux))
-        Sview[:, j, :, :, :] = apply_lowpass(U[j], wavelet_filters['phi'][0], J,  spatial_coefs)
-
-    if (m>1):
-        # Second order scattering coeffs
-        sec_order_coefs = J*(J-1)*L**2/2
-
-        S2norder = S[:,J*L+1:num_coefs,:,:] # view of the data
-        S2norder.shape = (num_signals, sec_order_coefs/L, L, spatial_coefs, spatial_coefs)
-
-        indx = 0
-        for j1 in np.arange(J):
-            Uj1 = np.fft.fft2(U[j1].view()) #U is in the spatial domain
-            for l1 in np.arange(Uj1.shape[1]):
-                Ujl1 = Uj1[:,l1,].view() #all images single angle, all spatial coefficients
-
-                for j2 in np.arange(j1+1,J):
-                    # | U_lambda1 conv Psi_lambda2 | conv phi
-                    aux = np.abs(np.fft.ifft2(apply_fourier_mult(Ujl1, wavelet_filters['psi'][j2])))
-                    #computing all angles at once
-                    S2norder[:,indx,:,:,:]= \
-                        apply_lowpass(aux, wavelet_filters['phi'][0], J,  spatial_coefs)
-
-                    indx = indx+1
-
-    #Rototranslation should use the V
-
-    return S,U
 """
