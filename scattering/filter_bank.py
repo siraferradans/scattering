7c8009352c63a60115113202a571b6db55194d53
siraferradans <sira.ferradans@gmail.com>
2016-08-23 13:34:50 +0200

    comments on the filter_bank with examples etc

--- scattering/filter_bank.py
+++ scattering/filter_bank.py
@@ -3,7 +3,22 @@ from skimage.filters import morlet_kernel, gabor_kernel
 
 
 def _zero_pad_filter(filter, N):
-    """ Zero pads 'filter' so it has a NxN size """
+    """ Zero pad filter to (N,N)
+    Zero pads 'filter' so it has a NxN size instead of its original size (n,n)
+
+    Parameters
+    ----------
+    filter : (n,n) ndarray
+        Filter in the Fourier domain of size (n,n)
+    N : int
+        Goal size of the filter, assumed to be squared (N,N)
+
+    Returns
+    -------
+    padded_filter : (N,N) ndarray
+        input filter 'filter' padded with zeros to the size (N,N)
+
+    """
 
     if filter.shape[0] > N :
         M = np.array(filter.shape[0])
@@ -24,6 +39,50 @@ def _zero_pad_filter(filter, N):
 
 
 def multiresolution_filter_bank_morlet2d(N, J=4, L=8, sigma_phi = 0.8, sigma_xi = 0.8):
+    """ Generates the multiresolution filter bank of 2D Morlet filters in the Fourier domain.
+       Computes a set of 2D Morlet filters at different scales and angles, plus a low pass filter. All of them at
+        at different resolutions.
+
+    Parameters
+    ----------
+    N : int
+        Size of the  filters, which are square (N,N)
+    J : int
+        Total number of scales of the filters located in the frequency domain as powers of 2.
+    L : int
+        Total number of angles per scale
+    sigma_phi : float
+        Standard deviation needed as a parameter for the low-pass filter (Gaussian), normally set to 0.8
+    sigma_xi  : float
+        Standard deviation needed as a parameter for every band-pass filter (Morlet), normally set to 0.8
+
+    Returns
+    -------
+    Filters : Dictionary structure with the filters saved in the Fourier domain organized in the following way
+            - Filters['phi'] : Low pass filter (Gaussian) in a 2D vector of size NxN
+            - Filters['psi'][resolution] : Band pass filter (Morlet) saved as 4D complex array of size [J,L,N,N]
+              where 'J' indexes the scale, 'L; the angles and NxN is the size of a single filter. The variable
+              'resolution' goes from 0 to J.
+
+    Examples
+    --------
+    Generate a multiresolution filterbank and plot all the bank pass filters at a given scale j, and angle l. They
+    will be shown in the Fourier domain.
+
+    >>>> N=128
+    >>>> J=5
+    >>>> L=8
+    >>>> j=3
+    >>>> l=5
+    >>>> wavelet_bnk,littlewood_p = multiresolution_filter_bank_morlet2d(px, J=J, L=L)
+    >>>> plt.figure(figsize=(18,6))
+    >>>> for r in np.arange(0,num_resolutions):
+            plt.subplot(1,num_resolutions,r+1)
+            f = wavelet_bnk['psi'][r][j][l,:,:]
+            plt.imshow(np.abs(np.fft.fftshift(f)))
+
+
+     """
 
     wf, littlewood = filter_bank_morlet2d(N, J=J, L=L, sigma_phi=sigma_phi, sigma_xi=sigma_xi)
 
@@ -33,19 +92,23 @@ def multiresolution_filter_bank_morlet2d(N, J=4, L=8, sigma_phi = 0.8, sigma_xi 
 
 
 def filter_bank_morlet2d(N, J=4, L=8, sigma_phi=0.8, sigma_xi=0.8):
-    """ Compute a 2D complex Morlet filter bank [1]_ in the Fourier domain.
+    """ Compute a 2D complex Morlet filter bank in the Fourier domain.
 
     Creates a filter bank of 1+JxL number of filters in the Fourier domain, where each filter has size NxN, and differ in
-    the activation frequency. All these filters are complex 2D morlet filters.
+    the activation frequency. All these filters are complex 2D morlet filters [1]_ , [2]_ .
 
     Parameters
     ----------
-    N : size of the (squared) filters
-    J : total number of scales of the filters which are located in the frequency domain, as powers of 2.
-    L : total number of angles for each scale
-    sigma_phi : standard deviation needed as a parameter for the low-pass filter (Gaussian)
-    sigma_xi  : standard deviation needed as a parameter for every band-pass filter (Morlet)
-
+    N : int
+        Size of the  filters, which are square (N,N)
+    J : int
+        Total number of scales of the filters located in the frequency domain as powers of 2.
+    L : int
+        Total number of angles per scale
+    sigma_phi : float
+        Standard deviation needed as a parameter for the low-pass filter (Gaussian), normally set to 0.8
+    sigma_xi  : float
+        Standard deviation needed as a parameter for every band-pass filter (Morlet), normally set to 0.8
 
     Returns
     -------
@@ -55,27 +118,43 @@ def filter_bank_morlet2d(N, J=4, L=8, sigma_phi=0.8, sigma_xi=0.8):
               where 'J' indexes the scale, 'L; the angles and NxN is the size of a single filter.
 
 
-    littlewood_paley : Sum
-    # tests the 'quality of the filters' by checking the littlewood-paley sum
-
-    # Output : dictionary with the filters (in the Fourier domain), and the littlewood-paley image
+    littlewood_paley : (N,N) ndarray
+        Measure of quality of the filter bank for image representation, it should be as close as possible to 1.
+        For more information see eq. 8 in [3]_
 
     References
     ----------
 
     .. [1] https://en.wikipedia.org/wiki/Filter_bank
+    .. [2] https://en.wikipedia.org/wiki/Discrete_wavelet_transform
+    .. [3] Bruna, J., Mallat, S. 'Invariant Scattering Convolutional Networks'. IEEE Transactions on PAMI, 2012.
 
-    More information on Wavelet Filter banks can be found:
-    https://en.wikipedia.org/wiki/Discrete_wavelet_transform
 
     Examples
     --------
-        >>>> J = 3, L=8, px = 32
-        >>>> wavelet_filters, littlewood = filter_bank_morlet2d(px, J=J, L=L, sigma_phi=0.6957,sigma_xi=0.8506 )
 
+    Generate a filter bank and show all the band pass filters in the Fourier domain.
 
+    >>>>J = 3
+    >>>>L=8
+    >>>>px = 32
+    >>>>wavelet_filters, littlewood = filter_bank_morlet2d(px, J=J, L=L, sigma_phi=0.6957,sigma_xi=0.8506 )
+    >>>>print('params consistent? J2=',J2,' L2=',L2,' N2=', N2)
+        print('Show low pass filter in the Fourier domain')
+        plt.imshow(np.abs(np.fft.fftshift(filters['phi'])))
+        plt.show()
+        print('.. and the band pass filters in the Fourier domain:')
 
-"""
+        for j in np.arange(0,J):
+            print('j=',j)
+            plt.figure(figsize=(18,6))
+            for l in np.arange(0,L):
+                plt.subplot(1, L, l+1)
+                plt.imshow(np.abs(np.fft.fftshift(filters['psi'][j][l, :, :])))
+
+            plt.show()
+
+    """
     max_scale = 2 ** (float(J - 1))
 
     sigma = sigma_phi * max_scale
@@ -134,16 +213,36 @@ def filter_bank_morlet2d(N, J=4, L=8, sigma_phi=0.8, sigma_xi=0.8):
     return Filters, littlewood_paley*2
 
 
+def filterbank_to_multiresolutionfilterbank(filters, max_resolution):
+    """ Converts a filter bank into a multiresolution filterbank
+    For every filter of a filter bank, compute this same filter at different resolutions (differnt sizes). The filters
+    are assumed to be in the Fourier domain, as well as the output multiresolution filters.
 
-def filterbank_to_multiresolutionfilterbank(filters,Resolution):
+    Parameters
+    ----------
+    filters : dictionary
+        Set of filters stored in a dictionary in the following way:
+         - filters['phi'] : Low pass filter (Gaussian) in a 2D vector of size NxN
+         - filters['psi'] : Band pass filter (Morlet) saved as 4D complex array of size [J,L,N,N]
+              where 'J' indexes the scale, 'L; the angles and NxN is the size of a single filter.
 
+    max_resolution : int
+        number of resolutions to compute for every filter (thus for every scale and angle)
+
+    Returns
+    -------
+    Filters_multires : dictionary
+        Set of filters in the Fourier domain, at different scales, angles and resolutions.
+        See multiresolution_filter_bank_morlet2d for more details on the Filters_multires structure.
+
+    """
     J = len(filters['psi']) #scales
     L = len(filters['psi'][0]) #angles
     N = filters['psi'][0].shape[-1] #size at max scale
 
     Phi_multires = []
     Psi_multires = []
-    for res in np.arange(0,Resolution):
+    for res in np.arange(0,max_resolution):
         Phi_multires.append(_get_filter_at_resolution(filters['phi'],res))
 
         aux_filt_psi = np.ndarray((J,L,int(N/2**res),int(N/2**res)), dtype='complex64')
@@ -158,12 +257,24 @@ def filterbank_to_multiresolutionfilterbank(filters,Resolution):
     return Filters_multires
 
 
-
 def _ispow2(N):
     return 0 == (N & (N - 1))
 
 
 def _get_filter_at_resolution(filt,j):
+    """Computes the filter 'filt' at resolution 'j'
+
+    Parameters
+    ----------
+    filt : ndarray
+        Filter in the Fourier domain.
+    j : int
+        Resolution to be computed
+    Returns
+    -------
+    filt_multires : ndarray
+        Filter 'filt' at the resolution j, in the Fourier domain
+    """
 
     cast = np.complex64
     N = filt.shape[0]  # filter is square
